b4396e6a389c3d24d85c7e2bd29267e8983946cf
mevic777 <kovalimarius@gmail.com>
2023-06-07 11:11:53 +0300

    [Changes]

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,34 @@
-$(document).ready(function () {
-  let container = $(".div");
+let user = sessionStorage.getItem("pop-up-key");
+
+if (!user) {
+  $(document).ready(function () {
+    $("#pop-up-modal").modal("show");
+    sessionStorage.setItem("pop-up-key", "true");
+  });
+}
+
+$(".btn").mouseover(function () {
+  let data = $(this).attr("data-btn");
+  let div = $(data);
+  let divs = $(".block");
+
+  if (div.hasClass("block-animation")) {
+    div.removeClass("block-animation");
+  } else {
+    divs.removeClass("block-animation");
+    div.addClass("block-animation");
+  }
+});
+
+$(".btn").mouseout(function () {
+  let data = $(this).attr("data-btn");
+  let div = $(data);
+  let divs = $(".block");
+
+  if (div.hasClass("block-animation")) {
+    div.removeClass("block-animation");
+  } else {
+    divs.removeClass("block-animation");
+    div.addClass("block-animation");
+  }
 });
--- home.php
+++ home.php
@@ -1,8 +1,10 @@
 <?php
+
 session_start();
 include 'includes/header.php';
 include 'db.php';
 include "helpers.php";
+
 ?>
 
 <main>
@@ -34,6 +36,46 @@ include "helpers.php";
             echo '<a class="link-secondary link-underline-opacity-0 m-1" href="/car?product=' . $car['product_link'] . '">' . $car['car_name'] . '</a>';
         } ?>
     </div>
+    <div class="container mt-3">
+        <div class="button-container mb-3">
+            <button class="btn btn-primary" data-btn="#first-block">First block</button>
+            <button class="btn btn-primary" data-btn="#second-block">Second block</button>
+            <button class="btn btn-primary" data-btn="#third-block">Third block</button>
+            <button class="btn btn-primary" data-btn="#fourth-block">Fourth block</button>
+        </div>
+        <div class="block-container" style="gap: 10px;">
+            <div class="col-md-3 block" id="first-block">
+                <p>1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, accusamus.</p>
+            </div>
+            <div class="col-md-3 block" id="second-block">
+                <p>2. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, tempore?</p>
+            </div>
+            <div class="col-md-3 block" id="third-block">
+                <p>3. Lorem ipsum dolor sit amet consectetur adipisicing elit. In, earum!</p>
+            </div>
+            <div class="col-md-3 block" id="fourth-block">
+                <p>4. Lorem ipsum dolor sit amet consectetur adipisicing elit. In, earum!</p>
+            </div>
+        </div>
+    </div>
 </main>
+
+<div class="modal fade" id="pop-up-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include 'includes/footer.php'; ?>
